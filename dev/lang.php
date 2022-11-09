6abf37296be7372e534a93b1d56c17a86a3571da
aXenDeveloper <axendeveloper@gmail.com>
2022-11-09 15:04:56 +0100

    Lesson 1

--- dev/lang.php
+++ dev/lang.php
@@ -1,5 +1,7 @@
 <?php
 
-$lang = array(
-	'__app_helloworld'	=> "Hello World"
-);
+$lang = [
+	'__app_helloworld'	=> "Hello World",
+	'module__helloworld_helloworld' => "My helloworld Module",
+	'hello_world_test_page' => "Wow! My first page in IPS! Hi :) for English"
+];
---
+++ modules/front/helloworld/pagetest.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace IPS\helloworld\modules\front\helloworld;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+
+if (!\defined('\IPS\SUITE_UNIQUE_KEY')) {
+	header((isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0') . ' 403 Forbidden');
+	exit;
+}
+
+/**
+ * pagetest
+ */
+class _pagetest extends \IPS\Dispatcher\Controller
+{
+	/**
+	 * Execute
+	 *
+	 * @return	void
+	 */
+	public function execute()
+	{
+
+		parent::execute();
+	}
+
+	/**
+	 * ...
+	 *
+	 * @return	void
+	 */
+	protected function manage()
+	{
+		\IPS\Output::i()->title = \IPS\Member::loggedIn()->language()->addToStack('hello_world_test_page');
+		\IPS\Output::i()->breadcrumb[] = [null, \IPS\Member::loggedIn()->language()->addToStack('hello_world_test_page')];
+		\IPS\Output::i()->output = \IPS\Theme::i()->getTemplate('pagetest', 'helloworld', 'front')->index();
+	}
+
+	// Create new methods with the same name as the 'do' parameter which should execute it
+}
